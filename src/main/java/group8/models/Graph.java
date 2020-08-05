b63dd8ff71cce67d67c0cc0fb606aebee7c665c5
raymondhonsumchiu <raymondhonsumchiu@gmail.com>
2020-08-05 21:28:39 +1200

    :construction: implemented barebones Graph class

--- src/main/java/group8/models/Graph.java
+++ src/main/java/group8/models/Graph.java
@@ -1,4 +1,16 @@
 package group8.models;
 
+import java.util.HashMap;
+
 public class Graph {
+    private HashMap<String, Node> _nodeMap = new HashMap<String, Node>();
+
+    /**
+     * Method used by GraphGenerator to add a new node into the Graph
+     * @param newNode node to add to the graph
+     */
+    public void addGraphNode(Node newNode){
+        String nodeID = newNode.getID();
+        _nodeMap.put(nodeID, newNode);
+    }
 }
