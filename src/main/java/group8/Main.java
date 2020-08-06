487317b18bba3d1b6887c283a8663709b9dcc8fc
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-06 19:46:58 +1200

    Remove tests

--- src/main/java/group8/Main.java
+++ src/main/java/group8/Main.java
@@ -7,8 +7,5 @@ import group8.services.GraphGenerator;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        GraphGenerator g = new GraphGenerator(new DOTParser());
-        Graph gg = g.getGraph("graph.gv");
     }
 }
