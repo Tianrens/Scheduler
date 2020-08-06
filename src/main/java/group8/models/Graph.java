04d3bd50935d511bce9b970193bd0fe74c752e81
raymondhonsumchiu <raymondhonsumchiu@gmail.com>
2020-08-06 20:13:26 +1200

    :construction: added getNode() method

--- src/main/java/group8/models/Graph.java
+++ src/main/java/group8/models/Graph.java
@@ -9,8 +9,18 @@ public class Graph {
      * Method used by GraphGenerator to add a new node into the Graph
      * @param newNode node to add to the graph
      */
-    public void addGraphNode(Node newNode){
+
+    public void addNode(Node newNode){
         String nodeID = newNode.getID();
         _nodeMap.put(nodeID, newNode);
+    }
+
+    /**
+     * Method used to obtain a specific node given its ID
+     * @param nodeID the node ID
+     * @return the node whose ID was given
+     */
+    public Node getNode(String nodeID){
+        return _nodeMap.get(nodeID);
     }
 }
