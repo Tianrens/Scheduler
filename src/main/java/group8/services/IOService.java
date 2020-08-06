3c73a8346c78e50acb30dac3b86fb7af8dfe76dc
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-06 15:34:19 +1200

    :sparkles: Add IOService class

---
+++ src/main/java/group8/services/IOService.java
@@ -0,0 +1,12 @@
+package group8.services;
+
+import java.util.List;
+
+public class IOService implements IOServiceInterface {
+
+
+    @Override
+    public List<String[]> getFileAsStringArray(String path) {
+        return null;
+    }
+}
