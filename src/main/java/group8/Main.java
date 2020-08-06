5f4ff3aee8071aa6bb56b20f0b6374362d7b0e34
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-06 19:37:13 +1200

    :bug: Loops through all lines of the .dot file now

--- src/main/java/group8/Main.java
+++ src/main/java/group8/Main.java
@@ -1,7 +1,14 @@
 package group8;
 
+import group8.models.Graph;
+import group8.services.DOTParser;
+import group8.services.GraphGenerator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        GraphGenerator g = new GraphGenerator(new DOTParser());
+        Graph gg = g.getGraph("graph.gv");
     }
 }
--- src/main/java/group8/services/GraphGenerator.java
+++ src/main/java/group8/services/GraphGenerator.java
@@ -28,28 +28,30 @@ public class GraphGenerator {
         Graph graph = new Graph();
 
         try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
-            String line = br.readLine();
+            String line;
 
-            List<String> graphData = _dotParser.parseStringLine(line);
+            while ((line = br.readLine()) != null) {
+                List<String> graphData = _dotParser.parseStringLine(line);
 
-            if (graphData.size() == 1) {
-                /** Graph name */
+                if (graphData.size() == 1) {
+                    /** Graph name */
 
-            } else if (graphData.size() == 2) {
-                /**WAITING ON IMPLEMENTATION
-                 *  graph.addNode(new Node(graphData[0], graphData[1]))
-                 */
+                } else if (graphData.size() == 2) {
+                    /**WAITING ON IMPLEMENTATION
+                     *  graph.addNode(new Node(graphData[0], graphData[1]))
+                     */
 
-            } else if (graphData.size() == 3) {
-                // The .dot file input can be assumed to be sequential. Therefore all nodes
-                // will have been previously initialised before they are referenced as an edge
+                } else if (graphData.size() == 3) {
+                    // The .dot file input can be assumed to be sequential. Therefore all nodes
+                    // will have been previously initialised before they are referenced as an edge
 
-                /**WAITING ON IMPLEMENTATION
-                 *  Node src = graph.getNode(graphData[0]);
-                 *  Node dst = graph.getNode(graphData[1]);
-                 *  src.addDestination(dst, graphData[2])
-                 *  dst.addParent(src)
-                 */
+                    /**WAITING ON IMPLEMENTATION
+                     *  Node src = graph.getNode(graphData[0]);
+                     *  Node dst = graph.getNode(graphData[1]);
+                     *  src.addDestination(dst, graphData[2])
+                     *  dst.addParent(src)
+                     */
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
