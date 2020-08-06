d84ad270e899150b02baf1d580d4312a347b774c
raymondhonsumchiu <raymondhonsumchiu@gmail.com>
2020-08-06 20:16:27 +1200

    :construction: comment out code until Node is implemented

--- src/main/java/group8/models/Graph.java
+++ src/main/java/group8/models/Graph.java
@@ -3,24 +3,30 @@ package group8.models;
 import java.util.HashMap;
 
 public class Graph {
+    /* Waiting on Node Implementation
     private HashMap<String, Node> _nodeMap = new HashMap<String, Node>();
+     */
 
     /**
      * Method used by GraphGenerator to add a new node into the Graph
      * @param newNode node to add to the graph
      */
 
+    /*
     public void addNode(Node newNode){
         String nodeID = newNode.getID();
         _nodeMap.put(nodeID, newNode);
     }
+     */
 
     /**
      * Method used to obtain a specific node given its ID
      * @param nodeID the node ID
      * @return the node whose ID was given
      */
+    /*
     public Node getNode(String nodeID){
         return _nodeMap.get(nodeID);
     }
+     */
 }
