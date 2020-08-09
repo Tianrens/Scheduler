46579ef20948e1439d0310c1b0d03b30c3951666
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-09 23:26:56 +1200

    :sparkle: New SimpleScheduler class

---
+++ src/main/java/group8/services/SimpleScheduler.java
@@ -0,0 +1,12 @@
+package group8.services;
+
+import group8.models.Graph;
+import group8.models.Schedule;
+
+public class SimpleScheduler implements IScheduler {
+
+    @Override
+    public Schedule generateValidSchedule(Graph graph) {
+        return null;
+    }
+}
