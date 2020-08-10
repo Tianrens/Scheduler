73fb1bd4ddf9447665ce6d10cb9156552ce36c89
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-10 22:17:03 +1200

    Replace with constant for default scheduler number processor

--- src/main/java/group8/scheduler/OneProcessorScheduler.java
+++ src/main/java/group8/scheduler/OneProcessorScheduler.java
@@ -9,6 +9,8 @@ import group8.models.TaskNode;
 
 import java.util.List;
 
+import static group8.scheduler.SchedulerConstants.*;
+
 public class OneProcessorScheduler implements IScheduler {
     private final TopologyFinder _topologyFinder;
 
@@ -22,13 +24,13 @@ public class OneProcessorScheduler implements IScheduler {
         int numProcessors = AppConfig.getInstance().get_numProcessors();
 
         if (numProcessors == 0) {
-            throw new AppConfigException();
+            return null;
         }
 
         Schedule schedule = new Schedule(numProcessors);
         schedule.setUnassignedTaskList(topology);
 
-        Processor processor = schedule.getProcessors().get(0); // Get first processor
+        Processor processor = schedule.getProcessors().get(ONE_PROCESSOR_SCHEDULER_DEFAULT); // Get first processor
         int startTime;
         for (TaskNode taskNode : topology) {
             startTime = processor.getFirstAvailableTime();
---
+++ src/main/java/group8/scheduler/SchedulerConstants.java
@@ -0,0 +1,5 @@
+package group8.scheduler;
+
+public class SchedulerConstants {
+    public static final int ONE_PROCESSOR_SCHEDULER_DEFAULT = 0;
+}
