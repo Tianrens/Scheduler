c61c3b3d69ee57735f9aa1dfe77456a86c1f91a0
raymondhonsumchiu <raymondhonsumchiu@gmail.com>
2020-08-11 18:22:09 +1200

    :white_check_mark: prepared @before and @after file management

--- src/test/java/group8/cli/CLITests.java
+++ src/test/java/group8/cli/CLITests.java
@@ -1,34 +1,53 @@
 package group8.cli;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class CLITests {
-//    @Test
-//    public void SimpleCommandLineArguments() throws CLIException {
-//        String[] args = new String[] {"inputFileTest.dot", "4", "-p", "8", "-v", "-o", "someOUTputDOTFile"};
-//        AppConfigBuilder cli = new AppConfigBuilder(args);
-//        AppConfig config = cli.build();
-//
-//        assertEquals("inputFileTest.dot", config.get_inputFile().toString());
-//        assertEquals(4, config.get_numProcessors());
-//        assertEquals(8, config.get_numCores());
-//        assertEquals(true, config.is_visualise());
-//        assertEquals("someOUTputDOTFile", config.get_outputFile().toString());
-//
-//    }
-//
-//    @Test
-//    public void DefaultCommandLineArguments() throws CLIException {
-//        String[] args = new String[] {"inputFileTest.dot", "4"};
-//        AppConfigBuilder cli = new AppConfigBuilder(args);
-//        AppConfig config = cli.build();
-//
-//        assertEquals("inputFileTest.dot" ,config.get_inputFile().toString());
-//        assertEquals(4, config.get_numProcessors());
-//        assertEquals(1, config.get_numCores());
-//        assertEquals(false, config.is_visualise());
-//        assertEquals("inputFileTest-output.dot", config.get_outputFile().toString());
-//
-//    }
+
+    @Before
+    public void init() throws IOException {
+        //new File("testResources").mkdirs();
+        File testFile = new File("inputFileTest.dot");
+        testFile.createNewFile();
+    }
+
+    @After
+    public void cleanUp(){
+        new File("inputFileTest.dot").delete();
+    }
+
+    @Test
+    public void SimpleCommandLineArguments() throws CLIException {
+        String[] args = new String[] {"inputFileTest.dot", "4", "-p", "8", "-v", "-o", "someOUTputDOTFile.dot"};
+        AppConfigBuilder cli = new AppConfigBuilder(args);
+        AppConfig config = cli.build();
+
+        assertEquals("inputFileTest.dot", config.get_inputFile().toString());
+        assertEquals(4, config.get_numProcessors());
+        assertEquals(8, config.get_numCores());
+        assertEquals(true, config.is_visualise());
+        assertEquals("someOUTputDOTFile.dot", config.get_outputFile().toString());
+
+    }
+
+    @Test
+    public void DefaultCommandLineArguments() throws CLIException {
+        String[] args = new String[] {"inputFileTest.dot", "4"};
+        AppConfigBuilder cli = new AppConfigBuilder(args);
+        AppConfig config = cli.build();
+
+        assertEquals("inputFileTest.dot" ,config.get_inputFile().toString());
+        assertEquals(4, config.get_numProcessors());
+        assertEquals(1, config.get_numCores());
+        assertEquals(false, config.is_visualise());
+        assertEquals("inputFileTest-output.dot", config.get_outputFile().toString());
+
+    }
 }
