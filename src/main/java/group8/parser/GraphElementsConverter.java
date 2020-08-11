e0b67737af4b73b07b333f899ababd4ead0af3ba
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-11 15:54:40 +1200

    Add graph generator implementation 2

---
+++ src/main/java/group8/parser/GraphElementsConverter.java
@@ -0,0 +1,11 @@
+package group8.parser;
+
+import group8.cli.AppConfigException;
+import group8.models.Graph;
+
+public class GraphElementsConverter implements IGraphGenerator {
+    @Override
+    public Graph generate() throws AppConfigException {
+        return null;
+    }
+}
