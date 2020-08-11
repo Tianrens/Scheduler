e93c0421b1711c6e41086102a1407dd8bb9262b6
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-11 23:44:38 +1200

    :hammer: Make all constants protected for all constants file

--- src/main/java/group8/parser/DOTFileConstants.java
+++ src/main/java/group8/parser/DOTFileConstants.java
@@ -1,7 +1,7 @@
 package group8.parser;
 
 public class DOTFileConstants {
-    public static final String WEIGHTATTR = "Weight";
-    public static final String STARTATTR = "Start";
-    public static final String PROCESSORATTR = "Processor";
+    protected static final String WEIGHTATTR = "Weight";
+    protected static final String STARTATTR = "Start";
+    protected static final String PROCESSORATTR = "Processor";
 }
