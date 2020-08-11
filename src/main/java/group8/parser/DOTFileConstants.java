63aaa7e4bb158f77fa5a8d76e1604f10aeb15560
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-11 17:18:22 +1200

    :sparkle: External parser resulting objects can now be converted into our own Graph object

---
+++ src/main/java/group8/parser/DOTFileConstants.java
@@ -0,0 +1,7 @@
+package group8.parser;
+
+public class DOTFileConstants {
+    public static final String WEIGHTATTR = "Weight";
+    public static final String STARTATTR = "Start";
+    public static final String PROCESSORATTR = "Processor";
+}
--- src/main/java/group8/parser/GraphExternalParserGenerator.java
+++ src/main/java/group8/parser/GraphExternalParserGenerator.java
@@ -2,14 +2,8 @@ package group8.parser;
 
 import com.paypal.digraph.parser.GraphEdge;
 import com.paypal.digraph.parser.GraphNode;
-import com.paypal.digraph.parser.GraphParser;
-import group8.cli.AppConfig;
-import group8.cli.AppConfigException;
 import group8.models.Graph;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import group8.models.TaskNode;
 import java.util.Map;
 
 public class GraphExternalParserGenerator implements IGraphGenerator {
@@ -20,8 +14,32 @@ public class GraphExternalParserGenerator implements IGraphGenerator {
     }
 
     @Override
-    public Graph generate() throws AppConfigException {
+    public Graph generate() {
         Graph graph = new Graph();
+        Map<String, GraphNode> nodes = _parser.getNodes();
+        Map<String, GraphEdge> edges = _parser.getEdges();
+
+        for (String nodeId : nodes.keySet()) {
+            GraphNode node = nodes.get(nodeId);
+
+            Integer weight = (Integer) node.getAttribute(DOTFileConstants.WEIGHTATTR);
+
+            graph.addNode(new TaskNode(weight, nodeId));
+        }
+
+        // The .dot file input can be assumed to be sequential. Therefore all nodes
+        // will have been previously initialised before they are referenced as an edge
+        for (String edgeId : edges.keySet()) {
+            GraphEdge edge = edges.get(edgeId);
+
+            TaskNode src = graph.getNode(edge.getNode1().getId());
+            TaskNode dst = graph.getNode(edge.getNode1().getId());
+            Integer weight = (Integer) edge.getAttribute(DOTFileConstants.WEIGHTATTR);
+
+            src.addDestination(dst, weight);
+            dst.addParentNode(src);
+        }
+
         return graph;
     }
 }
