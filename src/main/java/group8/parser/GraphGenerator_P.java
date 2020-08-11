1773d42bfb9e94f680359e9de9b35583a3dc81fd
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-11 16:14:13 +1200

    :hammer: refactor name for graphgenerator parser verison

--- src/main/java/group8/parser/GraphGenerator_P.java
+++ src/main/java/group8/parser/GraphGenerator_P.java
@@ -1,9 +1,10 @@
 package group8.parser;
 
+import com.paypal.digraph.parser.GraphParser;
 import group8.cli.AppConfigException;
 import group8.models.Graph;
 
-public class GraphElementsConverter implements IGraphGenerator {
+public class GraphGenerator_P implements IGraphGenerator {
     @Override
     public Graph generate() throws AppConfigException {
         return null;
