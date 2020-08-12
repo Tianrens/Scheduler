80b75f4ce2becc55ba8080d9e57bbaf0171b1225
Tianrens <tshe695@aucklanduni.ac.nz>
2020-08-12 15:58:44 +1200

    Simple graph generator test

--- src/test/java/group8/parser/ExternalGraphGeneratorTests.java
+++ src/test/java/group8/parser/ExternalGraphGeneratorTests.java
@@ -4,19 +4,29 @@ import group8.cli.AppConfig;
 import group8.cli.AppConfigException;
 import group8.models.Graph;
 import group8.models.TaskNode;
-import javafx.concurrent.Task;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ExternalGraphGeneratorTests {
 
     @Test
     public void DefaultGraphTest() {
+        Set<String> expectedOutput = new HashSet<>(Arrays.asList(
+                "node a 2",
+                "node b 3",
+                "node c 3",
+                "node d 2",
+                "edge a b 1",
+                "edge a c 10",
+                "edge b d 2",
+                "edge c d 1"
+        ));
+
         File inputFile = new File(this.getClass().getResource("defaultGraph.dot").getPath());
         AppConfig config = AppConfig.getInstance();
         config.setInputFile(inputFile);
@@ -31,12 +41,16 @@ public class ExternalGraphGeneratorTests {
         GraphExternalParserGenerator graphGenerator = new GraphExternalParserGenerator(parser);
         Graph inputGraph = graphGenerator.generate();
         List<TaskNode> arr = new ArrayList<>(inputGraph.getAllNodes().values());
-        for (TaskNode node : arr) {
-            System.out.println(node.getId()+node.getCost());
 
-            for (TaskNode edges : node.getEdgeList().keySet()) {
-                System.out.println(edges.getId());
+        Set<String> output = new HashSet<>();
+        for (TaskNode node : arr) {
+            output.add("node " + node.getId() + " " + node.getCost());
+
+            for (TaskNode edge : node.getEdgeList().keySet()) {
+                output.add("edge " + node.getId() + " " + edge.getId() + " " + node.getEdgeList().get(edge));
             }
         }
+
+        assertEquals(expectedOutput, output);
     }
 }
