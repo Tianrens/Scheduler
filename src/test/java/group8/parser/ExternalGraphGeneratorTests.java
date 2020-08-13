53da35039946e4e415721f19698059db5be81645
Tianrens <tshe695@aucklanduni.ac.nz>
2020-08-13 13:40:50 +1200

    More graph gen tests

--- src/test/java/group8/parser/ExternalGraphGeneratorTests.java
+++ src/test/java/group8/parser/ExternalGraphGeneratorTests.java
@@ -53,4 +53,108 @@ public class ExternalGraphGeneratorTests {
 
         assertEquals(expectedOutput, output);
     }
+
+    @Test
+    public void OneNodeGraph() {
+        Set<String> expectedOutput = new HashSet<>(Arrays.asList(
+                "node a 2"
+        ));
+        File inputFile = new File(this.getClass().getResource("OneNodeGraph.dot").getPath());
+        AppConfig config = AppConfig.getInstance();
+        config.setInputFile(inputFile);
+
+        DOTPaypalParser parser = null;
+        try {
+            parser = new DOTPaypalParser();
+            parser.getFileInputStream();
+        } catch (AppConfigException e) {
+            fail();
+        }
+        GraphExternalParserGenerator graphGenerator = new GraphExternalParserGenerator(parser);
+        Graph inputGraph = graphGenerator.generate();
+        List<TaskNode> arr = new ArrayList<>(inputGraph.getAllNodes().values());
+
+        Set<String> output = new HashSet<>();
+        for (TaskNode node : arr) {
+            output.add("node " + node.getId() + " " + node.getCost());
+
+            for (TaskNode edge : node.getEdgeList().keySet()) {
+                output.add("edge " + node.getId() + " " + edge.getId() + " " + node.getEdgeList().get(edge));
+            }
+        }
+
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void OneLevelGraph() {
+        Set<String> expectedOutput = new HashSet<>(Arrays.asList(
+                "node a 2",
+                "node b 3",
+                "node c 3",
+                "edge a b 1",
+                "edge a c 10"
+        ));
+        File inputFile = new File(this.getClass().getResource("OneLevelGraph.dot").getPath());
+        AppConfig config = AppConfig.getInstance();
+        config.setInputFile(inputFile);
+
+        DOTPaypalParser parser = null;
+        try {
+            parser = new DOTPaypalParser();
+            parser.getFileInputStream();
+        } catch (AppConfigException e) {
+            fail();
+        }
+        GraphExternalParserGenerator graphGenerator = new GraphExternalParserGenerator(parser);
+        Graph inputGraph = graphGenerator.generate();
+        List<TaskNode> arr = new ArrayList<>(inputGraph.getAllNodes().values());
+
+        Set<String> output = new HashSet<>();
+        for (TaskNode node : arr) {
+            output.add("node " + node.getId() + " " + node.getCost());
+
+            for (TaskNode edge : node.getEdgeList().keySet()) {
+                output.add("edge " + node.getId() + " " + edge.getId() + " " + node.getEdgeList().get(edge));
+            }
+        }
+
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void OneLineGraph() {
+        Set<String> expectedOutput = new HashSet<>(Arrays.asList(
+                "node a 2",
+                "node b 3",
+                "node c 3",
+                "edge a b 1",
+                "edge b c 10"
+        ));
+        File inputFile = new File(this.getClass().getResource("OneLineGraph.dot").getPath());
+        AppConfig config = AppConfig.getInstance();
+        config.setInputFile(inputFile);
+
+        DOTPaypalParser parser = null;
+        try {
+            parser = new DOTPaypalParser();
+            parser.getFileInputStream();
+        } catch (AppConfigException e) {
+            fail();
+        }
+        GraphExternalParserGenerator graphGenerator = new GraphExternalParserGenerator(parser);
+        Graph inputGraph = graphGenerator.generate();
+        List<TaskNode> arr = new ArrayList<>(inputGraph.getAllNodes().values());
+
+        Set<String> output = new HashSet<>();
+        for (TaskNode node : arr) {
+            output.add("node " + node.getId() + " " + node.getCost());
+
+            for (TaskNode edge : node.getEdgeList().keySet()) {
+                output.add("edge " + node.getId() + " " + edge.getId() + " " + node.getEdgeList().get(edge));
+            }
+        }
+
+        assertEquals(expectedOutput, output);
+    }
 }
