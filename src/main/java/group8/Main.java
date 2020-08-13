531f5107c096dcf0c569dc5d55702ddce3f37b1a
David Xiao <davidjinxiao@hotmail.com>
2020-08-13 16:29:10 +1200

    :green_heart: milestone 1 system build successful

--- src/main/java/group8/Main.java
+++ src/main/java/group8/Main.java
@@ -4,7 +4,12 @@ import group8.cli.AppConfig;
 import group8.cli.AppConfigBuilder;
 import group8.cli.AppConfigException;
 import group8.cli.CLIException;
+import group8.models.Schedule;
 import group8.parser.*;
+import group8.scheduler.IScheduler;
+import group8.scheduler.ITopologyFinder;
+import group8.scheduler.OneProcessorScheduler;
+import group8.scheduler.TopologyFinder;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,8 +22,12 @@ public class Main {
     public static void main(String[] args) throws AppConfigException {
         _appConfig = buildAppConfig(args);
 
-        IGraphGenerator graphGenerator = new GraphGenerator(new DOTDataParser());
         IGraphGenerator externalGraphGenerator = new GraphExternalParserGenerator(new DOTPaypalParser());
+        TopologyFinder topologyFinder = new TopologyFinder();
+        IScheduler scheduler = new OneProcessorScheduler(topologyFinder);
+        Schedule schedule = scheduler.generateValidSchedule(externalGraphGenerator.generate());
+        DOTDataParser outputBuilder = new DOTDataParser();
+        outputBuilder.parseOutput("",schedule);
     }
 
     private static AppConfig buildAppConfig(String[] args) {
