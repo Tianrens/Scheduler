faaa18c8da6f178a58679cab1ee0d8f3654b07c2
David Xiao <davidjinxiao@hotmail.com>
2020-08-17 17:00:40 +1200

    ProcessorExcpetion comments

--- src/main/java/group8/models/ProcessorException.java
+++ src/main/java/group8/models/ProcessorException.java
@@ -1,5 +1,8 @@
 package group8.models;
 
+/**
+ * Exception for any problem related to {@link Processor}
+ */
 public class ProcessorException extends Exception {
     public ProcessorException() {
         super();
