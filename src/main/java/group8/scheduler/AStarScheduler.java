5fa3170fbb1e5e196e8e7bb3ba406e060376cf1f
jteo97 <55283692+jteo97@users.noreply.github.com>
2020-08-19 20:21:51 +1200

    :construction: implement a check for complete schedule

--- src/main/java/group8/scheduler/AStarScheduler.java
+++ src/main/java/group8/scheduler/AStarScheduler.java
@@ -2,28 +2,39 @@ package group8.scheduler;
 
 import group8.cli.AppConfigException;
 import group8.models.Graph;
+import group8.models.Node;
 import group8.models.ProcessorException;
 import group8.models.Schedule;
 import group8.scheduler.IScheduler;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class AStarScheduler implements IScheduler {
     private PriorityQueue<Schedule> _openState = new PriorityQueue<>();
     private List<Schedule> _closedState = new ArrayList<>();
+    private Graph _graph;
 
     @Override
     public Schedule generateValidSchedule(Graph graph) throws ProcessorException, AppConfigException {
+
         return null;
     }
 
-    public boolean checkCompleteSchedule(Schedule state) {
-        return false;
+    private boolean checkCompleteSchedule(Schedule state) {
+        HashMap<String, Node> allNodesOfGraph = _graph.getAllNodes();
+        Set<String> nodeIdList = allNodesOfGraph.keySet();
+
+        Set<String> taskIdList = state.getTasks().keySet();
+        taskIdList.removeAll(nodeIdList);
+
+        if (taskIdList.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private int calculateF(Schedule state) {
+    private int calculateCostFunction(Schedule state) {
         return -1;
     }
 }
