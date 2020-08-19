98fd67f2dd9131503c7a1f43c98f43bd98ae9551
raymondhonsumchiu <raymondhonsumchiu@gmail.com>
2020-08-19 14:23:20 +1200

    :sparkle: implemented a simple heuristic that finds the estimate from the state to the end

--- src/main/java/group8/algorithm/SimpleHeuristic.java
+++ src/main/java/group8/algorithm/SimpleHeuristic.java
@@ -1,4 +1,40 @@
 package group8.algorithm;
 
+import group8.models.Node;
+import group8.models.Schedule;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class implementing a simple heuristic that calculates an estimate cost directly from a state to
+ * the final schedule
+ */
 public class SimpleHeuristic implements IHeuristic {
+
+    /**
+     * Method for calculating an estimate based on the sum of all unassigned node costs
+     * @param state state we are calculating the estimate for
+     * @param allNodes list of all nodes in the graph
+     * @return the cost from the state to the end
+     */
+    @Override
+    public int calculateEstimate(Schedule state, HashMap<String, Node> allNodes) {
+        int totalCost = 0;
+        //Map<String, int[]> stateNodes = state.get_nodes();
+        ArrayList<String> stateKeys = new ArrayList<String>(state.get_nodes().keySet());
+        ArrayList<String> allKeys = new ArrayList<String>(allNodes.keySet());
+        //List<Node> statelessNodes = new ArrayList<>();
+
+        // Sum up the costs of all nodes not in the current state
+        for (String nodeId: allKeys) {
+            if(!stateKeys.contains(nodeId)){
+                totalCost += allNodes.get(nodeId).getCost();
+            }
+        }
+
+        return totalCost;
+    }
 }
