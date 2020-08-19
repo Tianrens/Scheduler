8e9f831ac2b29c8bd6bd15a046d2cf9cfd40ca79
Tianrens <tshe695@aucklanduni.ac.nz>
2020-08-19 14:11:47 +1200

    Changes for Jennifer

--- src/main/java/group8/models/Schedule.java
+++ src/main/java/group8/models/Schedule.java
@@ -33,7 +33,7 @@ public class Schedule {
      */
     public Schedule() {
         for (int i = 0; i < AppConfig.getInstance().getNumProcessors(); i++) {
-            _processors[i] = -1;
+            _processors[i] = 0;
         }
     }
 
@@ -43,11 +43,8 @@ public class Schedule {
      * @param startTime the first value of array
      * @param processor the second value of array
      */
-    public void setNode(String nodeId, int startTime, int processor) throws ScheduleException {
+    public void setNode(String nodeId, int startTime, int processor) {
         int[] value = new int[]{startTime, processor};
-//        if (_nodes.containsKey(nodeId)) {
-//            throw new ScheduleException("NodeId already in map.");
-//        }
         _nodes.put(nodeId, value);
     }
 
@@ -55,34 +52,9 @@ public class Schedule {
      * Set the start time of a processor
      * @param processor the processor. Processors start at 0 index!
      * @param startTime the start time of the processor.
-     * @throws ScheduleException
      */
-    public void setProcessorStartTime(int processor, int startTime) throws ScheduleException {
-//        int oldStartTime = _processors[processor];
-//        // Can remove for memory.
-//        if (oldStartTime > startTime) {
-//            throw new ScheduleException("Invalid Start time of processor");
-//        }
+    public void setProcessorStartTime(int processor, int startTime) {
         _processors[processor] = startTime;
-    }
-
-    /**
-     * Calculates the start time of each processor in _processors.
-     * Iterates through the _nodes map, and calcualtes the start time of each processor.
-     * @throws ScheduleException
-     */
-    protected void computeProcessorStartTimes() throws ScheduleException {
-        for (String key : _nodes.keySet()) {
-//            // Can remove for memory.
-//            if (_nodes.get(key) == null) {
-//                throw new ScheduleException("Null Value for this key, in Nodes HashMap");
-//            }
-            int[] value = _nodes.get(key);
-            int processorId = value[0];
-            int startTime = value[1];
-
-            setProcessorStartTime(processorId, startTime);
-        }
     }
 
 
--- src/main/java/group8/models/ScheduleException.java
+++
@@ -1,7 +0,0 @@
-package group8.models;
-
-public class ScheduleException extends Exception {
-    public ScheduleException(String m) {
-        super(m);
-    }
-}
