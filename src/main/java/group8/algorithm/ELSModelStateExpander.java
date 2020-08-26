e2bfa10c95581cb9384a26522207a3570ba7ebfe
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-26 18:22:38 +1200

    unrefactor to avoid merge conflicts

--- src/main/java/group8/algorithm/ELSModelStateExpander.java
+++ src/main/java/group8/algorithm/ELSModelStateExpander.java
@@ -58,106 +58,105 @@ public class ELSModelStateExpander implements IStateExpander, Callable<List<Sche
 
             //if no parents then node has no dependencies and can be assigned to the schedule
             if(node.getParentNodeList().size()==0){
-                noParentsSetUp(processors, node, scheduledNodes, newSchedules);
+                boolean emptyAssign = false; //checks for duplicate states, where a node sis assigned to an empty process
+
+                int loopCount = processors.length;
+                if (node.getIdenticalNodeId() != -1) {
+                    int difference = _graph.getGroupOfIdenticalNodes(node.getIdenticalNodeId()).size() - processors.length;
+                    if (difference > 0) {
+                        loopCount += difference;
+                    }
+                }
+
+                for(int i = 0 ; i < loopCount ; i++) {
+                    if (node.getIdenticalNodeId() != -1) {
+                        node = _graph.getFixedOrderNode(node.getIdenticalNodeId()); // will alwyas
+                    }
+
+                    int[] newProcessors = makeProcessorList(processors);
+                    Map<String, int[]> newScheduledNodes = new HashMap<>();
+                    int[] nodeInfo = new int[2];
+
+                    if(!emptyAssign && newProcessors[i]==-1){
+                        emptyAssign=true;
+                        newProcessors[i]=0;
+                    }else if(emptyAssign && newProcessors[i]==-1){
+                        //This might need reconsideration, because I dont think there will be a case where after
+                        // reading an empty process will we come across a process that is not empty
+                        continue;
+                    }
+
+                    nodeInfo[0] = newProcessors[i];
+                    nodeInfo[1]=i;
+                    newProcessors[i]=newProcessors[i]+node.getCost();
+
+                    newScheduledNodes.putAll(scheduledNodes);
+                    newScheduledNodes.put(node.getId(), nodeInfo);
+
+                    newSchedules.add(assignSchedule(newProcessors,newScheduledNodes));
+                }
             } else if(checkParents(node.getParentNodeList(),scheduledNodes)){
-                withParentsSetUp(processors, node, scheduledNodes, newSchedules);
+                //checks for duplicate states, where a node sis assigned to an empty process
+                boolean emptyAssign = false;
+
+                int loopCount = processors.length;
+                if (node.getIdenticalNodeId() != -1) {
+                    int difference = _graph.getGroupOfIdenticalNodes(node.getIdenticalNodeId()).size() - processors.length;
+                    if (difference > 0) {
+                        loopCount += difference;
+                    }
+                }
+
+                for(int i = 0 ; i < loopCount ; i++){
+                    if (node.getIdenticalNodeId() != -1) {
+                        node = _graph.getFixedOrderNode(node.getIdenticalNodeId());
+                    }
+
+                    int[] newProcessors = makeProcessorList(processors);
+                    Map<String, int[]> newScheduledNodes = new HashMap<>();
+                    int[] nodeInfo = new int[2];
+
+                    if(!emptyAssign && processors[i]==-1){
+                        emptyAssign=true;
+                        newProcessors[i]=0;
+
+                    }else if(emptyAssign && processors[i]==-1){
+                        //This might need reconsideration, because I dont think there will be a case where after
+                        // reading an empty process will we come across a process that is not empty
+                        continue;
+                    }
+
+                    int startTime;
+                    int earliestStartTime = 0;
+
+                    for(Node parent : node.getParentNodeList()) {
+                        if(scheduledNodes.get(parent.getId())[1]!=i){
+                            startTime = parent.getEdgeList().get(node)+parent.getCost()+scheduledNodes.get(parent.getId())[0];
+                            if(startTime < processors[i]){
+                                startTime = processors[i];
+                            }
+                        }else {
+                            startTime = processors[i];
+                        }
+
+                        if(startTime>earliestStartTime){
+                            earliestStartTime=startTime;
+                        }
+                    }
+
+                    nodeInfo[0] = earliestStartTime;
+                    nodeInfo[1]=i;
+                    newProcessors[i] = earliestStartTime + node.getCost();
+                    newScheduledNodes.putAll(scheduledNodes);
+                    newScheduledNodes.put(node.getId(), nodeInfo);
+
+                    newSchedules.add(assignSchedule(newProcessors,newScheduledNodes));
+                }
             }
 
         }
         return newSchedules;
     }
-
-    private void noParentsSetUp(int[] processors, Node node, Map<String, int[]> scheduledNodes, List<Schedule> newSchedules) throws AppConfigException {
-        boolean emptyAssign = false; //checks for duplicate states, where a node sis assigned to an empty process
-
-        for(int i = 0 ; i < processors.length ; i++) {
-            if (node.getIdenticalNodeId() != -1) {
-                node = _graph.getFixedOrderNode(node.getIdenticalNodeId()); // will alwyas
-            }
-
-            int[] newProcessors = makeProcessorList(processors);
-            Map<String, int[]> newScheduledNodes = new HashMap<>();
-            int[] nodeInfo = new int[2];
-
-            if(!emptyAssign && newProcessors[i]==-1){
-                emptyAssign=true;
-                newProcessors[i]=0;
-            }else if(emptyAssign && newProcessors[i]==-1){
-                //This might need reconsideration, because I dont think there will be a case where after
-                // reading an empty process will we come across a process that is not empty
-                continue;
-            }
-
-            nodeInfo[0] = newProcessors[i];
-            nodeInfo[1]=i;
-            newProcessors[i]=newProcessors[i]+node.getCost();
-
-            newScheduledNodes.putAll(scheduledNodes);
-            newScheduledNodes.put(node.getId(), nodeInfo);
-
-            newSchedules.add(assignSchedule(newProcessors,newScheduledNodes));
-        }
-    }
-
-    private void withParentsSetUp(int[] processors, Node node, Map<String, int[]> scheduledNodes, List<Schedule> newSchedules) throws AppConfigException {
-        //checks for duplicate states, where a node sis assigned to an empty process
-        boolean emptyAssign = false;
-
-        int loopCount = processors.length;
-        if (node.getIdenticalNodeId() != -1) {
-            int difference = _graph.getGroupOfIdenticalNodes(node.getIdenticalNodeId()).size() - processors.length;
-            if (difference > 0) {
-                loopCount += difference;
-            }
-        }
-
-        for(int i = 0 ; i < loopCount ; i++){
-            if (node.getIdenticalNodeId() != -1) {
-                node = _graph.getFixedOrderNode(node.getIdenticalNodeId());
-            }
-
-            int[] newProcessors = makeProcessorList(processors);
-            Map<String, int[]> newScheduledNodes = new HashMap<>();
-            int[] nodeInfo = new int[2];
-
-            if(!emptyAssign && processors[i]==-1){
-                emptyAssign=true;
-                newProcessors[i]=0;
-
-            }else if(emptyAssign && processors[i]==-1){
-                //This might need reconsideration, because I dont think there will be a case where after
-                // reading an empty process will we come across a process that is not empty
-                continue;
-            }
-
-            int startTime;
-            int earliestStartTime = 0;
-
-            for(Node parent : node.getParentNodeList()) {
-                if(scheduledNodes.get(parent.getId())[1]!=i){
-                    startTime = parent.getEdgeList().get(node)+parent.getCost()+scheduledNodes.get(parent.getId())[0];
-                    if(startTime < processors[i]){
-                        startTime = processors[i];
-                    }
-                }else {
-                    startTime = processors[i];
-                }
-
-                if(startTime>earliestStartTime){
-                    earliestStartTime=startTime;
-                }
-            }
-
-            nodeInfo[0] = earliestStartTime;
-            nodeInfo[1]=i;
-            newProcessors[i] = earliestStartTime + node.getCost();
-            newScheduledNodes.putAll(scheduledNodes);
-            newScheduledNodes.put(node.getId(), nodeInfo);
-
-            newSchedules.add(assignSchedule(newProcessors,newScheduledNodes));
-        }
-    }
-
 
     /**
      * helper method to check if all parents have been assigned
