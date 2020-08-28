b1851c8f4dad3d5bd66a02bc96ea52fb9b8734ae
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-08-28 19:33:17 +1200

    :white_check_mark: finish test 1

--- src/test/java/group8/algorithm/IdenticalNodeDuplicationTests.java
+++ src/test/java/group8/algorithm/IdenticalNodeDuplicationTests.java
@@ -7,38 +7,89 @@ import group8.models.Node;
 import group8.models.Schedule;
 import group8.parser.*;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
-public class IdenticalNodeDuplicationTests {
-    private Graph _graph;
-    private IStateExpander _stateExpander;
-    private Schedule _schedule;
-    private IDOTFileWriter _writer;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
 
-    @Before
-    public void setUp() throws AppConfigException {
+public class IdenticalNodeDuplicationTests {
+    private static Graph _graph;
+    private static IStateExpander _stateExpander;
+    private static IDOTFileWriter _writer;
+
+    private static Schedule _schedule;
+    private static List<String> _correctSchedules;
+
+    @BeforeClass
+    public static void setUp() throws AppConfigException {
+        AppConfig.getInstance().setInputFile(new File(IdenticalNodeDuplicationTests.class.getResource("identicalNodes.dot").getPath()));
+        AppConfig.getInstance().setNumProcessors(3);
         _writer = new DOTFileWriter();
 
-        AppConfig.getInstance().setInputFile(new File(this.getClass().getResource("identicalNodes.dot").getPath()));
-        AppConfig.getInstance().setNumProcessors(3);
         IGraphGenerator externalGraphGenerator = new GraphExternalParserGenerator(new DOTPaypalParser());
         _graph = externalGraphGenerator.generate();
+                Schedule empty = new Schedule();
+        _graph.setHeuristicCost(Math.min(new SimpleHeuristic().calculateEstimate(empty, _graph.getAllNodes()),new GreedyHeuristic().calculateEstimate(empty, _graph.getAllNodes())));
+
+        _stateExpander = new ELSModelStateExpander(_graph);
 
         _schedule = new Schedule();
         _schedule.scheduleTask("a", 0, 0);
         _schedule.setProcessorStartTime(0, 2);
+    }
 
-        _stateExpander = new ELSModelStateExpander(_graph);
+    @BeforeClass
+    public static void setUpCorrectSchedules() {
+        _correctSchedules = new ArrayList<>();
+        _correctSchedules.add(
+                "\ta [Weight=2, Start=0, Processor=1];" + System.lineSeparator() +
+                "\tb [Weight=3, Start=2, Processor=1];" + System.lineSeparator()
+        );
+
+        _correctSchedules.add(
+                        "\td [Weight=3, Start=4, Processor=2];" + System.lineSeparator() +
+                        "\ta [Weight=2, Start=0, Processor=1];" + System.lineSeparator()
+        );
+
+        _correctSchedules.add(
+                "\ta [Weight=2, Start=0, Processor=1];" + System.lineSeparator() +
+                "\tc [Weight=4, Start=2, Processor=1];" + System.lineSeparator()
+        );
+
+        _correctSchedules.add(
+                "\ta [Weight=2, Start=0, Processor=1];" + System.lineSeparator() +
+                "\tc [Weight=4, Start=4, Processor=2];" + System.lineSeparator()
+        );
+    }
+
+    @Test
+    public void correctIdenticalGrouping() {
+        assertEquals(_graph.getNode("b").getIdenticalNodeId(), 0);
+        assertEquals(_graph.getNode("d").getIdenticalNodeId(), 0);
+
+        assertEquals(_graph.getNode("a").getIdenticalNodeId(), -1);
+        assertEquals(_graph.getNode("c").getIdenticalNodeId(), -1);
+        assertEquals(_graph.getNode("e").getIdenticalNodeId(), -1);
     }
 
     @Test
     public void generateSchedule() throws AppConfigException {
         List<Schedule> schedules = _stateExpander.getNewStates(_schedule);
         for (Schedule s : schedules) {
-            _writer.writeOutputToConsole(s,_graph);
+            String scheduleResult = _writer.writeOutputToString(s,_graph);
+            scheduleResult = scheduleResult.substring(24, scheduleResult.indexOf("->")-2);
+            if (_correctSchedules.contains(scheduleResult)) {
+                _correctSchedules.remove(scheduleResult);
+            }
+        }
+
+        if (_correctSchedules.size() != 0) {
+            fail();
         }
     }
 }
