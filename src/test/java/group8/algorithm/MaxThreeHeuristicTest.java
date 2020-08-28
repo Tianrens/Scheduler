a39b39618b5e0422c0490dcce5b21dd8af775285
jteo97 <55283692+jteo97@users.noreply.github.com>
2020-08-28 21:50:53 +1200

    :white_check_mark: add base template for max three heuristic test

---
+++ src/test/java/group8/algorithm/MaxThreeHeuristicTest.java
@@ -0,0 +1,18 @@
+package group8.algorithm;
+
+import group8.cli.AppConfig;
+import group8.cli.AppConfigException;
+import group8.models.Node;
+import group8.models.Schedule;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * This class tests the MaxThreeHeuristic Class.
+ */
+public class MaxThreeHeuristicTest {
+
+}
