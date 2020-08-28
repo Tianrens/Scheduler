a2f93091f3803ca6f17ee7186a4df5a774f850e0
jteo97 <55283692+jteo97@users.noreply.github.com>
2020-08-28 21:59:12 +1200

    :white_check_mark: add test for empty schedule

--- src/test/java/group8/algorithm/MaxThreeHeuristicTest.java
+++ src/test/java/group8/algorithm/MaxThreeHeuristicTest.java
@@ -14,5 +14,24 @@ import static org.junit.Assert.*;
  * This class tests the MaxThreeHeuristic Class.
  */
 public class MaxThreeHeuristicTest {
+    /**
+     * Tests on an empty schedule
+     * @throws AppConfigException
+     */
+    @Test
+    public void MaxThreeTest() throws AppConfigException {
+
+        AppConfig.getInstance().setNumProcessors(1);
+        MaxThreeHeuristic maxThreeHeuristic = new MaxThreeHeuristic();
+        HashMap<String, Node> nodes = new HashMap<>();
+        nodes.put("A", new Node(5, "A"));
+        nodes.put("B", new Node(10, "B"));
+
+        Schedule schedule = new Schedule();
+        double result = maxThreeHeuristic.calculateEstimate(schedule, nodes);
+
+        assertEquals(result, 15, 1);
+    }
+
 
 }
