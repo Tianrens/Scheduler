4751c1ab6cac2bf400da4d9b62b9a10c0e09d4e7
Tianrens <tshe695@aucklanduni.ac.nz>
2020-08-28 20:00:30 +1200

    Tests

--- src/test/java/group8/algorithm/GreedyHeuristicTest.java
+++ src/test/java/group8/algorithm/GreedyHeuristicTest.java
@@ -25,4 +25,33 @@ public class GreedyHeuristicTest {
 
         assertEquals(result, 15, 1);
     }
+
+    @Test
+    public void GreedyTest2() throws AppConfigException {
+        AppConfig.getInstance().setNumProcessors(1);
+        GreedyHeuristic greedy = new GreedyHeuristic();
+        HashMap<String, Node> nodes = new HashMap<>();
+        nodes.put("A", new Node(5, "A"));
+        nodes.put("B", new Node(1, "B"));
+
+        Schedule schedule = new Schedule();
+        double result = greedy.calculateEstimate(schedule, nodes);
+
+        assertEquals(result, 6, 1);
+    }
+
+    @Test
+    public void GreedyTest3() throws AppConfigException {
+        AppConfig.getInstance().setNumProcessors(1);
+        GreedyHeuristic greedy = new GreedyHeuristic();
+        HashMap<String, Node> nodes = new HashMap<>();
+        nodes.put("A", new Node(5, "A"));
+        nodes.put("B", new Node(1, "B"));
+
+        Schedule schedule = new Schedule();
+        schedule.scheduleTask("A", 0, 0);
+        double result = greedy.calculateEstimate(schedule, nodes);
+
+        assertEquals(result, 1, 1);
+    }
 }
